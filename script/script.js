95fcfbc8be050b426fa84fb07094cf1daebd60ed
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-05-12 20:32:18 -0300

    test - tentando outro tipo de botão de menu.

--- script/script.js
+++
@@ -1,6 +0,0 @@
-const hb = document.getElementById ('hb');
-const nav2 = document.getElementById ('nav2');
-hb.addEventListener('click', () => {
-    hb.classList.toggle('open');
-    nav2.classList.toggle('open');
-});
