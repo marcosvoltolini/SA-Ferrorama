78f5b489d65792bd5c4022dfcbb8ad9f69e7d8af
marcosvoltolini <marcos_voltolini-@estudante.sesisenai.org.br>
2026-09-15 08:44:14 -0300

    up - arrumei o link

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
             <span id="burguer" class="material-icons">menu</span>
         </nav>
         <nav class="navs">
-            <button class="botao"><a id="lk" href="rota_trem.php">Rota dos trens</a></button>
-            <button class="botao"><a id="lk" href="horario_trem.php">Horários</a></button>
+            <button class="botao"><a id="lk" href="Rota_trem.php">Rota dos trens</a></button>
+            <button class="botao"><a id="lk" href="Horario_trem.php">Horários</a></button>
             <button class="botao"><a id="lk" href="Listar_trens.php">Ver trens</a></button>
         </nav>
     </header>
