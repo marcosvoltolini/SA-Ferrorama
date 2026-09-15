1392950e43a7b869d14676045a62f7e7e92de56e
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-09-15 09:39:28 -0300

    add - horarios inicio

--- public/Horario_trem.php
+++ public/Horario_trem.php
@@ -1,14 +1,41 @@
 <?php
 
 ?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>horarios dos trens</title>
+    <title>Red Rush!</title>
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
-<body>
-    
-</body>
-</html>
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
 
+<body>
+    <header>
+        <nav class="nave">
+            <img class="logo" src="../assets/imag/Logo_red_rush.png" alt="Red Rush Logo">
+            <h1 id="hs">Red Rush</h1>
+            <span id="burguer" class="material-icons">menu</span>
+        </nav>
+        <nav class="navs">
+            <button class="botao"><a id="lk" href="public/Rota_trem.php">Rota dos trens</a></button>
+            <button class="botao"><a id="lk" href="public/Horario_trem.php">Trens disponiveis</a></button>
+        </nav>
+    </header>
+
+    <main>
+        <div class="bens">
+            <img class="jacobite" src="../assets/imag/jacobite.png" alt="jacobite">
+        
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
