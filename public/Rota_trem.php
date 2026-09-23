a20d4dfc4bd6230a1d82929ef7295c04e3205869
rebeca-sarrea <rebeca_sarrea@estudante.sesisenai.org.br>
2026-09-23 10:57:12 -0300

    add - rota trens (falta css)

---
+++ public/Rota_trem.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Red Rush!</title>
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+
+<body>
+    <header>
+        <nav class="nave">
+            <img class="logo" src="../assets/imag/Logo_red_rush.png" alt="Red Rush Logo">
+            <h1 id="hs"><a href="../index.php">Red Rush</a></h1>
+            <span id="burguer" class="material-icons">menu</span>
+        </nav>
+        <nav class="navs">
+            <button class="botao"><a id="lk" href="public/Rota_trem.php">Rota dos trens</a></button>
+            <button class="botao"><a id="lk" href="public/Horario_trem.php">Trens disponiveis</a></button>
+        </nav>
+    </header>
+
+    <main>
+    <div class= "bensgrand">
+            <div class= "bens">
+            <img  src="../assets/imag/jacobite.png" alt="jacobite">
+            <h1 class= imgrota>Jacobite</h1>
+            <img  src="../assets/imag/jacobite-rota.png" alt="jacobite">
+    </div>
+            <div class= "bens">
+                <img src="../assets/imag/Rocky.png" alt="rocky">
+                <h1 class= imgrota>Rocky</h1>
+                <img  src="../assets/imag/Rocky_Mountaineer_Routes.png" alt="rocky">
+            </div>
+
+            <div class= bens>
+                <img src="../assets/imag/orient.png" alt="orinet">
+                <h5 class= imgrota>Venice Simplon-Orient-Express</h5>
+                <img  src="../assets/imag/venice rota.png" alt="orinet">
+            </div>
+
+            <div class= "bens">
+                <img src="../assets/imag/bernina.png" alt="bernina">
+                <h3 class= imgrota>Bernina Express</h3>
+                <img  src="../assets/imag/bernina-rota.png" alt="bernina">
+            </div>
+   
+        
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
